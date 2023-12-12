4a975449791d23eaaa9de2335418c0e371da3008
TecHeReTiC3141 <talex73@mail.ru>
2023-12-12 11:47:09 +0300

    DaisyUI has been set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                darkTheme: {
+                    "primary": "#059669",
+                    "secondary": "#009800",
+                    "accent": "#00f800",
+                    "neutral": "#050701",
+                    "base-100": "#2d1f2a",
+                    "info": "#27cdff",
+                    "success": "#5eb82e",
+                    "warning": "#d39900",
+                    "error": "#ff0048",
+                },
+                lightTheme: {
+                    "primary": "#3b82f6",
+                    "secondary": "#00d440",
+                    "accent": "#047857",
+                    "neutral": "#0c1109",
+                    "base-100": "#fff2f1",
+                    "info": "#00e1ff",
+                    "success": "#00c892",
+                    "warning": "#ea580c",
+                    "error": "#f43f5e",
+                },
+            },
+        ],
     },
-  },
-  plugins: [],
 }
 export default config
