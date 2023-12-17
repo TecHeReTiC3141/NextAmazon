7df5033e150a48aaef24055805fb80325d6a59c7
TecHeReTiC3141 <talex73@mail.ru>
2023-12-17 14:56:35 +0300

    Product card appearance has been finished

---
+++ app/ui/components/PriceTag.tsx
@@ -0,0 +1,13 @@
+import formatPrice from "@/app/lib/utils/formatPrice";
+
+interface PriceTagProps {
+    price: number,
+    className?: string,
+}
+
+export default function PriceTag({price, className}: PriceTagProps) {
+    return (
+        <span className={`badge ${className}`}>{formatPrice(price)}</span>
+    );
+}
+
--- app/ui/components/ProductCard.tsx
+++ app/ui/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import {Product} from "@prisma/client";
 import Link from "next/link";
 import Image from "next/image";
 import formatPrice from "@/app/lib/utils/formatPrice";
+import PriceTag from "@/app/ui/components/PriceTag";
 
 interface ProductCardProps {
     product: Product
@@ -9,13 +10,21 @@ interface ProductCardProps {
 
 
 export default function ProductCard({product}: ProductCardProps) {
+    const isNew = Date.now() - new Date(product.createdAt).getTime() <= 7 * 86400 * 1000;
     return (
         <Link href={`/products/${product.id}`} className="card w-full bg-base-100 hover:shadow-xl transition-shadow">
+            <figure>
+                <Image src={product.imageUrl}
+                       alt={product.name}
+                       width={800}
+                       height={400}
+                className="object-cover h-48"></Image>
+            </figure>
             <div className="card-body">
                 <h2 className="card-title">{product.name}</h2>
-                <Image src={product.imageUrl} alt={product.name} width={700} height={400}></Image>
+                {isNew && <h2 className="badge badge-secondary">NEW</h2>}
                 <p>{product.description}</p>
-                <p>{formatPrice(product.price)}</p>
+                <PriceTag price={product.price} />
             </div>
         </Link>
     )
