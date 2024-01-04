331ffc1164d48c39666ac659ac2aed976daf09e2
TecHeReTiC3141 <talex73@mail.ru>
2024-01-04 13:24:40 +0300

    Function for merging local and user carts has been created

--- app/lib/db/cart.ts
+++ app/lib/db/cart.ts
@@ -1,6 +1,6 @@
 import prisma from "@/app/lib/db/prisma";
 import {cookies} from "next/headers";
-import {Cart, Prisma} from "@prisma/client";
+import {Cart, CartItem, Prisma} from "@prisma/client";
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 
@@ -73,4 +73,70 @@ export async function createCart(): Promise<ShoppingCart> {
         size: 0,
         subtotal: 0,
     };
+}
+
+export async function mergeAnonymousCartIntoUsersCart(userId: string) {
+    const localCartId = cookies().get("localCartId")?.value;
+
+    const localCart = localCartId ? await prisma.cart.findUnique({
+        where: {id: localCartId,},
+        include: {items: true,}
+    }) : null;
+
+    if (!localCart) return;
+
+    const userCart = await prisma.cart.findFirst({
+        where: {userId},
+        include: {items: true},
+    });
+
+
+    await prisma.$transaction(async tx => {
+        if (userCart) {
+            const mergedItems = mergeCartItems(localCart.items, userCart.items);
+
+            await tx.cartItem.deleteMany({
+                where: {cartId: localCart.id},
+            });
+
+            await tx.cartItem.createMany({
+                data: mergedItems.map(item => ({
+                    cartId: userCart.id,
+                    quantity: item.quantity,
+                    productId: item.productId,
+                }))
+            });
+        } else {
+            await tx.cart.create({
+                data: {
+                    userId,
+                    items: {
+                        createMany: {
+                            data: localCart.items.map(
+                                ({id, cartId, ...item}) => item
+                            ),
+                        }
+                    }
+                },
+            });
+        }
+
+        await tx.cart.delete({where: {id: localCartId}});
+        cookies().set("localCartId", "");
+    });
+
+}
+
+function mergeCartItems(...cartItems: CartItem[][]): CartItem[] {
+    return cartItems.reduce((acc, items) => {
+        items.forEach(item => {
+            const existingItem = acc.find(it2 => it2.productId === item.productId);
+            if (existingItem) {
+                existingItem.quantity += item.quantity;
+            } else {
+                acc.push(item);
+            }
+        });
+        return acc;
+    }, [] as CartItem[]);
 }
