be661f246cf1b2ed8a601263239e1f3daf9e2a58
TecHeReTiC3141 <talex73@mail.ru>
2024-01-04 17:50:25 +0300

    Problem with authOptions has been removed

---
+++ app/lib/configs/authOptions.ts
@@ -0,0 +1,66 @@
+import {NextAuthOptions} from "next-auth";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import prisma from "@/app/lib/db/prisma";
+import {PrismaClient} from "@prisma/client";
+import {Adapter} from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import {env} from "@/app/lib/env";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import {mergeAnonymousCartIntoUsersCart} from "@/app/lib/db/cart";
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
+    providers: [
+        GoogleProvider({
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET,
+        }),
+        GithubProvider({
+            clientId: env.GITHUB_CLIENT_ID,
+            clientSecret: env.GITHUB_CLIENT_SECRET,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "email",
+                    placeholder: "example@mail.com",
+                },
+                password: {
+                    label: "Password",
+                    type: "password",
+                },
+            },
+            async authorize(credentials) {
+                if (!credentials || !credentials.email || !credentials.password) {
+                    return null;
+                }
+
+                const user = await prisma.user.findFirst({
+                    where: {email: credentials.email},
+                });
+                console.log("found user", user, await bcrypt.compare( credentials.password, user?.password || "",));
+                if (user && await bcrypt.compare( credentials.password, user.password || "")) {
+                    const {id,  password, ...userFields} = user;
+                    console.log("logged", user);
+                    return user;
+                }
+                return null;
+            }
+        }),
+    ],
+    callbacks: {
+        session({session, user}) {
+            session.user.id = user.id;
+            return session;
+        },
+    },
+    events: {
+        async signIn({user}) {
+            await mergeAnonymousCartIntoUsersCart(user.id);
+        }
+    }
+}
